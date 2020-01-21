92cfe6e395e3ea331fdb21eae31c8e991ce12335
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-01-21 14:19:35 -0600

    Completed
    Completed the iterative_sorting.py.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,21 +6,27 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
+        for e in range(i + 1, len(arr)):
+            if arr[e] < arr[smallest_index]:
+                smallest_index = e     
 
         # TO-DO: swap
-
-
-
+        elem1 = arr[i]
+        elem2 = arr[smallest_index]
+        arr[i] = elem2
+        arr[smallest_index] = elem1
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    bubbles = len(arr)
+    
+    for i in range(bubbles):
+        for e in range(0, bubbles-i-1):
+            if arr[e] > arr[e+1]:
+                arr[e], arr[e+1] = arr[e+1], arr[e]
     return arr
 
 
